8f204cd8f784f70f1fa4d28a40c22255cd9f6a51
bdi-ist <bdi-ist@yandex.ru>
2024-07-21 19:39:35 +0500

    Функции высшего порядка

---
+++ 10-array-clean/app.js
@@ -0,0 +1,20 @@
+
+const customFilter = (removeFn, inputArray) => {
+    const result = [];
+    for (let i = 0; i < inputArray.length; i++) {
+        if (!removeFn(inputArray[i])) {
+            result.push(inputArray[i]);
+        } else {
+            console.log(`Необходимо ли удалить элемент ${inputArray[i]}? ` + true);
+        }
+        //возможно как-то по другому надо было, но вроде по логике "если не больше 5 - записываем, в противном - true на удалении, или надо было отдельной функцией?"
+    }
+    return result;
+};
+
+const removeMoreThanFive = (number) => number > 5;
+const inputArray = [3, 10, 1, 2];
+
+const outputArray = customFilter(removeMoreThanFive, inputArray);
+
+console.log(outputArray);
